4edb2b36ad4153d60ba0344e821eaa8e9c747dd2
Zearain <martinba88@gmail.com>
2025-10-04 20:08:15 +0200

    added doctor and clock

---
+++ Scenes/PhotoFrame/DrHenryMorrison/DrHenryMorrison.cs
@@ -0,0 +1,45 @@
+using System;
+using Godot;
+
+public partial class DrHenryMorrison : Node3D
+{
+    [Node("Camera3D")]
+    private Camera3D _camera;
+
+    [Node("InteractableObject")]
+    private InteractableObject _interactableObject;
+
+    [Node("Camera3D/CameraStateMonitor")]
+    private CameraStateMonitor _cameraStateMonitor;
+
+    public override void _Ready()
+    {
+        _interactableObject.InteractableObjectClicked += OnInteractableObjectClicked;
+        _cameraStateMonitor.CameraActivated += OnCameraActivated;
+        _cameraStateMonitor.CameraDeactivated += OnCameraDeactivated;
+    }
+
+    public override void _ExitTree()
+    {
+        _interactableObject.InteractableObjectClicked -= OnInteractableObjectClicked;
+        _cameraStateMonitor.CameraActivated -= OnCameraActivated;
+        _cameraStateMonitor.CameraDeactivated -= OnCameraDeactivated;
+    }
+
+    private void OnInteractableObjectClicked()
+    {
+        _camera.MakeCurrent();
+    }
+
+    private void OnCameraActivated()
+    {
+        // Disable collision when this frame's camera is active
+        _interactableObject.SetCollisionEnabled(false);
+    }
+
+    private void OnCameraDeactivated()
+    {
+        // Re-enable collision when this frame's camera is inactive
+        _interactableObject.SetCollisionEnabled(true);
+    }
+}
