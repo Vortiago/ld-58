8b49efe0ab2bc5a9a37c961124744adabc460fc0
Atle H. Havsø <atle@havso.net>
2025-10-04 02:04:27 +0200

    Better code!

--- Main.cs
+++ Main.cs
@@ -7,34 +7,12 @@ public partial class Main : Node3D
 	[Export] public Camera3D CameraRed {get; set;}
 	
 	public override void _Ready() {
-		ConnectPhotoFrames();
 	}
 	
-	private void ConnectPhotoFrames() {
-		var redFrame = GetNode<Area3D>("PhotoFrames/Frame1/Area3D");
-		redFrame.InputEvent += (camera, @event, pos, normal, shape) => {
-			if (@event is InputEventMouseButton mouse && mouse.Pressed) {
-				SwitchToView(0);
-				GD.Print("Switching to 0");
-			}
-		};
-		
-		var blueFrame = GetNode<Area3D>("PhotoFrames/Frame2/Area3D");
-		blueFrame.InputEvent += (camera, @event, pos, normal, shape) => {
-			if (@event is InputEventMouseButton mouse && mouse.Pressed) {
-				SwitchToView(1);
-				GD.Print("Switching to 1");
-			}
-		};
-	}
-	
-	private void SwitchToView(int viewIndex) {
+	public void FrameClicked(Camera3D associatedCamera) {
 		CameraBlue.Current = false;
 		CameraRed.Current = false;
 		
-		switch (viewIndex) {
-			case 0: { CameraRed.Current = true; break; }
-			case 1: { CameraBlue.Current = true; break; }
-		}
+		associatedCamera.Current = true;
 	}
 }
