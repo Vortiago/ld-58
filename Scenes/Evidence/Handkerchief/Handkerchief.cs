5e46b98b186395bbad31ffa338112b67906baf42
Zearain <martinba88@gmail.com>
2025-10-05 12:49:57 +0200

    added dropped ledger

---
+++ Scenes/Evidence/Handkerchief/Handkerchief.cs
@@ -0,0 +1,24 @@
+using System;
+using Godot;
+
+public partial class Handkerchief : Node3D
+{
+
+    [Node("InteractableObject")]
+    private InteractableObject _interactableObject;
+
+    public override void _Ready()
+    {
+        _interactableObject.InteractableObjectClicked += OnInteractableObjectClicked;
+    }
+
+    public override void _ExitTree()
+    {
+        _interactableObject.InteractableObjectClicked -= OnInteractableObjectClicked;
+    }
+
+    private void OnInteractableObjectClicked()
+    {
+        GD.Print("Handkerchief clicked");
+    }
+}
