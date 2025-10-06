b99a5db8be3a7611018b23d42124597ea37a0a22
Atle H. Havsø <atle@havso.net>
2025-10-06 16:20:38 +0200

    Implement AboutDialog with content and show/hide functionality; add About button to StartScreen

---
+++ Scenes/AboutDialog/AboutDialog.cs
@@ -0,0 +1,82 @@
+using Godot;
+using System;
+
+public partial class AboutDialog : PanelContainer
+{
+	[Export] private Button CloseButton;
+	[Export] private RichTextLabel ContentLabel;
+
+	public override void _Ready()
+	{
+		CloseButton.Pressed += OnCloseButtonPressed;
+		Visible = false;
+
+		// Set the about content
+		SetContent();
+	}
+
+	public override void _ExitTree()
+	{
+		CloseButton.Pressed -= OnCloseButtonPressed;
+	}
+
+	private void SetContent()
+	{
+		if (ContentLabel == null) return;
+
+		ContentLabel.BbcodeEnabled = true;
+		ContentLabel.Text = @"[b][font_size=24]Inspector Crawford and the Hallway Murder[/font_size][/b]
+
+[b]Game Jam Entry[/b]
+Ludum Dare 58 - Theme: Collector
+
+[b]Developed by:[/b]
+Vortiago and Zearan
+
+[b]Asset Attributions:[/b]
+• Art:
+	- Model and textures by Ulf
+	- Nevrax SARL / Winch Gate Properties Ltd. Ryzom. https://atys.wiki.ryzom.com/wiki/Ryzom_Commons:About
+• Background music:
+	Victorian Loop by Joe Baxter-Webb (BossLevelVGM)
+
+[b]Godot Engine:[/b]
+This game uses Godot Engine, available under the following license:
+
+Copyright (c) 2014-present Godot Engine contributors.
+Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the ""Software""), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.";
+	}
+
+	public new void Show()
+	{
+		Visible = true;
+	}
+
+	public new void Hide()
+	{
+		Visible = false;
+	}
+
+	private void OnCloseButtonPressed()
+	{
+		Hide();
+	}
+}
--- Scenes/StartScreen/StartScreen.cs
+++ Scenes/StartScreen/StartScreen.cs
@@ -6,21 +6,30 @@ public partial class StartScreen : CanvasLayer
 	[Signal] public delegate void GameStartedEventHandler();
 
 	[Export] private Button StartButton;
+	[Export] private Button AboutButton;
+	[Export] private PanelContainer AboutDialog;
 
 	public override void _Ready()
 	{
 		StartButton.Pressed += OnStartButtonPressed;
+		AboutButton.Pressed += OnAboutButtonPressed;
 	}
 
 	public override void _ExitTree()
 	{
 		StartButton.Pressed -= OnStartButtonPressed;
+		AboutButton.Pressed -= OnAboutButtonPressed;
 	}
 
 	private void OnStartButtonPressed()
 	{
 		EmitSignal(SignalName.GameStarted);
 		Hide();
+	}
+
+	private void OnAboutButtonPressed()
+	{
+		AboutDialog?.Show();
 	}
 
 	public new void Show()
